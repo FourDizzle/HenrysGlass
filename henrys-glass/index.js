20324f6192c60fb81885533c8eded2c30b4a62cc
Nicholas Cassiani <nick@spinsci.com>
2017-11-30 00:43:32 -0600

    added api route

--- henrys-glass/index.js
+++ henrys-glass/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 
-// app.get('/', (req, res) => res.send('Hello World!'))
+var router = express.Router();
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+app.use('/api', router);
+
 app.use(express.static('public'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
