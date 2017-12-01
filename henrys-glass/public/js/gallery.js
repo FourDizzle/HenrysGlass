b7c04576237f5acc555eb4b7a07ca3e41ae8d963
Nicholas Cassiani <nick@spinsci.com>
2017-12-01 07:38:27 -0600

    gallery

--- henrys-glass/public/js/gallery.js
+++ henrys-glass/public/js/gallery.js
@@ -6,14 +6,21 @@
 
   $('#gallery a').on('click', function(event) {
     var imgSrc = $(this).attr('href');
-    var lightboxImg = $('#gallery-image img');
-    var lightboxHeight = window.innerHeight - 120;
+    var lightboxImg = $('#gallery-modal img');
+    var modalDialog = $('#gallery-modal .modal-dialog');
+    var modalBody = $('#gallery-modal .modal-body');
+    var heightDiff = 55 + parseInt(modalDialog.css('margin-top')) * 2
+      + parseInt(modalBody.css('padding-top')) * 2;
+    console.log(heightDiff);
+    var imgHeight = window.innerHeight - heightDiff;
     var galleryModal = $('#gallery-modal');
 
     event.preventDefault();
 
     lightboxImg.attr('src', imgSrc);
-    lightboxImg.css({height: lightboxHeight + 'px'});
+    lightboxImg.css({height: imgHeight + 'px'});
+    // lightboxImg.css({height: '100%',});
+    // $('#gallery-modal .modal-content').css({height: lightboxHeight + 'px'});
     galleryModal.modal({show: true,});
   });
 }(window, jQuery));
